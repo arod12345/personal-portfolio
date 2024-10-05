2584557bcb4933d8e036a0d6635abecbbf44289a
sisayabel681 <sisayabel681@gmail.com>
2024-10-05 18:15:05 +0000

    first commit

---
+++ src/sections/Hero.jsx
@@ -0,0 +1,51 @@
+import React, { Suspense } from "react";
+import { Canvas } from "@react-three/fiber";
+import { PerspectiveCamera } from "@react-three/drei";
+import HackerRoom from "../components/HackerRoom.jsx";
+import CanvasLoader from "../components/CanvasLoader.jsx";
+import { useMediaQuery } from "react-responsive";
+import { calculateSizes } from "../constants/index.js";
+import Target from "../components/Target.jsx";
+
+const Hero = () => {
+
+  const isSmall = useMediaQuery({ maxWidth: 440 })
+  const isMobile = useMediaQuery({ maxWidth: 768 })
+  const isTablet = useMediaQuery({ minWidth: 768, maxWidth: 1024 });
+
+  const sizes = calculateSizes(isSmall, isTablet, isMobile)
+
+  return (
+    <section className="min-h-screen w-full flex flex-col relative ">
+      <div className="w-full mx-auto flex flex-col sm:mt-36 mt-20 c-space gap-3">
+        <p className="sm:text-3xl text-2xl font-bold text-white text-center">
+          Hi I am Abel <span className="waving-hand">✋</span>
+        </p>
+        <p className="hero_tag text-gray_gradient text-center">
+          Buildings Products & Brands
+        </p>
+      </div>
+
+      <div className="w-full h-full absolute insert-0">
+        {/* <Leva /> */}
+        <Canvas className="w-full h-full">
+          <Suspense fallback={<CanvasLoader />}>
+            <PerspectiveCamera makeDefault position={[0, 0, 20]} />
+            <HackerRoom
+              position={sizes.deskPosition}
+              rotation={[3.4, 0.2, -3.2]}
+              scale={sizes.deskScale}
+            />
+            <group>
+              <Target position={sizes.targetPosition} />
+            </group>
+            <ambientLight intensity={1} />
+            <directionalLight position={[10, 10, 10]} intensity={0.5} />
+          </Suspense>
+        </Canvas>
+      </div>
+    </section>
+  );
+};
+
+export default Hero;
