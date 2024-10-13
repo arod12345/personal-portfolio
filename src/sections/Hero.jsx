59906aa7afc8b68991f4a8ea3b48cc25c40559fd
sisayabel681 <sisayabel681@gmail.com>
2024-10-13 14:10:24 +0000

    fixed the main model

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,7 +1,7 @@
 import React, { Suspense } from "react";
 import { Canvas } from "@react-three/fiber";
 import { PerspectiveCamera } from "@react-three/drei";
-import HackerRoom from "../components/HackerRoom.jsx";
+import CyberPunk from "../components/CyberPunk.jsx";
 import CanvasLoader from "../components/CanvasLoader.jsx";
 import ReactLogo from "../components/ReactLogo.jsx"
 import Cube from "../components/Cube.jsx"
@@ -12,8 +12,46 @@ import HeroCamera from "../components/HeroCamera.jsx";
 import { useMediaQuery } from "react-responsive";
 import { calculateSizes } from "../constants/index.js";
 import Target from "../components/Target.jsx";
-
+import { useControls,Leva } from 'leva'
 const Hero = () => {
+
+  const x=useControls("CyberPunk",{
+    positionX:{
+      value:2.5,
+      min:-10,
+      max:10
+    },
+    positionY:{
+      value:2.5,
+      min:-10,
+      max:10
+    },
+    positionZ:{
+      value:2.5,
+      min:-10,
+      max:10
+    },
+    rotationX:{
+      value:0,
+      min:-10,
+      max:10
+    },
+    rotationY:{
+      value:0,
+      min:-10,
+      max:10
+    },
+    rotationZ:{
+      value:0,
+      min:-10,
+      max:10
+    },
+    scale:{
+      value:1,
+      min:0.1,
+      max:10
+    }
+  })
 
   const isSmall = useMediaQuery({ maxWidth: 440 })
   const isMobile = useMediaQuery({ maxWidth: 768 })
@@ -23,25 +61,18 @@ const Hero = () => {
 
   return (
     <section className="min-h-screen w-full flex flex-col relative ">
-      <div className="w-full mx-auto flex flex-col sm:mt-36 mt-20 c-space gap-3">
-        <p className="sm:text-3xl text-2xl font-bold text-white text-center">
-          Hi I am Abel <span className="waving-hand">✋</span>
-        </p>
-        <p className="hero_tag text-gray_gradient text-center">
-          Buildings Products & Brands
-        </p>
-      </div>
+      
 
       <div className="w-full h-full absolute insert-0">
-        {/* <Leva /> */}
+        <Leva />
         <Canvas className="w-full h-full">
           <Suspense fallback={<CanvasLoader />}>
             <PerspectiveCamera makeDefault position={[0, 0, 20]} />
             <HeroCamera isMobile={isMobile}>
-              <HackerRoom
-                position={sizes.deskPosition}
-                rotation={[3.4, 0.2, -3.2]}
-                scale={sizes.deskScale}
+              <CyberPunk
+                position={[x.positionX,x.positionY,x.positionZ]}
+                rotation={[x.rotationX,x.rotationY,x.rotationZ]}
+                scale={[x.scale,x.scale,x.scale]}
               />
             </HeroCamera>
 
@@ -57,9 +88,19 @@ const Hero = () => {
           </Suspense>
         </Canvas>
       </div>
-      <div className="absolute bottom-7 left-0 right-0 w-full z-10 c-space">
+
+      <div className="w-full mx-auto flex flex-col sm:mt-36 mt-20 c-space gap-3">
+        <p className="sm:text-3xl text-2xl font-bold text-white text-center">
+          Hi I am Abel <span className="waving-hand">✋</span>
+        </p>
+        <p className="hero_tag text-gray_gradient text-center">
+          Buildings Products & Brands
+        </p>
+      </div>
+      
+      <div className="absolute bottom-7 left-0 right-0 w-full z-10 bg-transparent">
         <a href="#contact" className="w-fit">
-          <Button name="Let's work together" isBean containerClass="sm:w-fit w-full sm:min-w-96" />
+          <Button name="Scroll Down"  containerClass="sm:w-fit w-full sm:min-w-96" />
         </a>
       </div>
     </section>
